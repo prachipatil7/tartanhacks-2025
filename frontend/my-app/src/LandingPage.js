7b3f443ddfea085b19729d0d852c27fbf475e908
prachipatil7 <62350419+prachipatil7@users.noreply.github.com>
2025-02-08 01:59:07 -0500

    fix landing page function call start

--- frontend/my-app/src/LandingPage.js
+++ frontend/my-app/src/LandingPage.js
@@ -4,14 +4,17 @@ import { useNavigate } from "react-router-dom";
 
 console.log("Google Maps API Key:", process.env.REACT_APP_GOOGLE_MAPS_API_KEY);
 
-const LandingPage = () => {
+const LandingPage = ({ onStart }) => {
   const navigate = useNavigate();
 
   return (
     <div className="landing-container">
       <h1>Passenger Princess</h1>
       <p>Turn on the car, babe! So ready for the ride :D</p>
-      <button className="start-button" onClick={() => navigate("/navigation")}>
+      <button className="start-button" onClick={() => {
+        onStart()
+        navigate("/navigation")
+      }}>
         Let's Go!
       </button>
     </div>
