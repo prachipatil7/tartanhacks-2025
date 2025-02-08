42ae83d0f807ea5dba8023985417cd8fa7b3a824
sukritirawal1 <rawalsukriti@gmail.com>
2025-02-08 15:51:41 -0500

    Updated Nearby Landmark Info

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,25 +1,54 @@
 from pydantic import BaseModel
 from typing import List, Optional
-
-
-class Location(BaseModel):
-    lat: float
-    lng: float
-    name: Optional[str] = None
-    address: Optional[str] = None
-
-
-class Direction(BaseModel):
-    start_location: Location
-    end_location: Location
-    distance: str
-    instructions: str
+from .maps import (
+    get_directions,
+    get_all_route_steps,
+    create_route_with_stop,
+    Location,
+    Step,
+    extract_turn_landmarks
+)
+from .t2v import synthesize_text
 
 
 class TripStatus(BaseModel):
     start: Location
     dest: Location
+    curr: Location
     duration: str
     distance: str
-    route: Optional[List[Direction]] = []
+    route: Optional[List[Step]] = []
     landmarks: Optional[List[Location]] = []
+
+    def model_post_init(self, _):
+        directions = get_directions(self.start, self.dest)
+        # print(directions)
+        self.route = get_all_route_steps(directions)
+        self.route.reverse()
+
+    def check_route_instruction(self):
+        if self.curr == self.route[0].start_location:
+            step = self.route.pop(0)
+            step = extract_turn_landmarks([step])
+            if step.landmark_name is None:
+                return synthesize_text(step.instructions)
+            else:
+                return synthesize_text(step.instructions + "when you see " + step.landmark_name)
+        else:
+            return None
+
+    def update_status(self):
+        directions = get_directions(self.curr, self.dest)
+        route = directions["legs"][0]
+        self.duration = route["duration"]["text"]
+        self.distance = route["distance"]["text"]
+        self.route = get_all_route_steps(directions)
+
+    def add_stop(self, keyword, location_type):
+        directions = create_route_with_stop(
+            self.curr, self.dest, keyword, location_type
+        )
+        route = directions["legs"][0]
+        self.duration = route["duration"]["text"]
+        self.distance = route["distance"]["text"]
+        self.route = get_all_route_steps(directions)
