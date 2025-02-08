b60c0dd21ff161a135fe3e0eea44d7f584ce04b2
prachipatil7 <62350419+prachipatil7@users.noreply.github.com>
2025-02-07 23:08:27 -0500

    websocket functionality

--- frontend/my-app/src/App.js
+++ frontend/my-app/src/App.js
@@ -14,16 +14,60 @@ function LandingPage({ onStart }) {
 }
 
 function App() {
+  let socket
+  let recognition
+
+
+  function setupAudioCapture() {
+    socket = new WebSocket('http://127.0.0.1:8000/ws');
+
+    socket.onopen = () => {
+      console.log('WebSocket connection established');
+    };
+
+    socket.onmessage = (event) => {
+      const response = event.data;
+      console.log("Recieved:", response)
+    };
+
+    function sendTranscriptToServer(transcript) {
+      socket.send(transcript);
+    }
+
+
+    recognition = new (window.SpeechRecognition || window.webkitSpeechRecognition)();
+    recognition.continuous = true; // Keeps listening
+    recognition.interimResults = false; // Provides real-time results
+
+    recognition.onresult = (event) => {
+      let transcript = event.results[event.results.length - 1][0].transcript;
+      console.log("User said:", transcript);
+      sendTranscriptToServer(transcript)
+    };
+
+    recognition.start(); // Start listening
+
+    // Handle errors
+    recognition.onerror = (event) => {
+      console.error("Error occurred:", event.error);
+    };
+  }
+
+  function start() {
+    alert("Navigation Starting!")
+    setupAudioCapture()
+  }
+
   return (
     <div className="app">
-      <LandingPage onStart={() => alert("Navigation Starting!")} />
+      <LandingPage onStart={() => start()} />
 
       {/* GIF Display */}
       <div className="gif-container">
         <img src={glitterGif} alt="Animated GIF" className="side-gif" />
       </div>
     </div>
-  );
+  )
 }
 
 export default App;
